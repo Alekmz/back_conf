e1a663c0af7bf0749880ab16ef698d22abe1dc44
jborges42 <joao.borges@redacaonline.com.br>
2023-01-19 21:27:35 -0300

    started migrations and any controllers

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name', 127);
+            $table->string('last_name', 255);
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->tinyInteger('is_admin')->default(0);
+            $table->tinyInteger('is_vaccinated')->default(0);
+            $table->string('birth_date', 10)->nullable(false);
+            $table->string('document', 11)->nullable();
+            $table->foreignId('club_id')->constrained('clubs')->onUpdate('cascade')->onDelete('restrict');
+            $table->tinyInteger('local_accommodation')->default(0);
+            $table->tinyInteger('is_present')->default(0);
+            $table->tinyInteger('use_medication')->default(0);
+            $table->string('password', 255);
             $table->rememberToken();
             $table->timestamps();
         });
